175bdb95c3efaedaaa1f3978b33f3a9fa885eba8
Aisha <aishasalimg@gmail.com>
2024-08-16 22:38:34 -0500

    push

--- app/firebase.js
+++ app/firebase.js
@@ -6,16 +6,16 @@ import dotenv from 'dotenv';
 // Load environment variables from .env file
 dotenv.config();
 
-// Your web app's Firebase configuration using environment variables
 const firebaseConfig = {
-  apiKey: "AIzaSyD0yl3kH1U5oD0XFJLEifwXEy5rEPlUL9A",
-  authDomain: "flashcardsaas-72583.firebaseapp.com",
-  projectId: "flashcardsaas-72583",
-  storageBucket: "flashcardsaas-72583.appspot.com",
-  messagingSenderId: "376186688812",
-  appId: "1:376186688812:web:c2400d858efbd504cf0889",
-  measurementId: "G-7PC3ZTKJ25"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
